ec2d0367da45d0e4633f34dd9412bdaef7219f7b
sharonLin <sharonlin5963@gmail.com>
2025-07-03 03:34:48 +0800

    firebase config

---
+++ app/firebase/config.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKUrSG2JF9fR04MRbev_qzRHVq40J55go",
+  authDomain: "jailulu-store-769a0.firebaseapp.com",
+  projectId: "jailulu-store-769a0",
+  storageBucket: "jailulu-store-769a0.firebasestorage.app",
+  messagingSenderId: "448350652423",
+  appId: "1:448350652423:web:d3ca622a4e00e95bfcc2a2",
+  measurementId: "G-K8W0HBV8SC",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export { app, auth };
